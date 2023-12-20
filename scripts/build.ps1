cc6da084f39eb06fe732ff16d5b26d3cd960b713
Myriad-Dreamin <camiyoru@gmail.com>
2023-12-21 01:00:22 +0800

    dev: extract meta before build

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,3 +1,4 @@
+cargo run --release --bin mitex-cli
 cargo build --release --target wasm32-unknown-unknown -p mitex-typst
 rm packages/mitex/mitex.wasm -Force
 mv target/wasm32-unknown-unknown/release/mitex_typst.wasm packages/mitex/mitex.wasm
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,4 @@
+cargo run --release --bin mitex-cli
 cargo build --release --target wasm32-unknown-unknown -p mitex-typst
 rm -f packages/mitex/mitex.wasm
 mv target/wasm32-unknown-unknown/release/mitex_typst.wasm packages/mitex/mitex.wasm
