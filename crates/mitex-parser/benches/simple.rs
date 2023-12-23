74d0b167b2b20810440fe308e12efbcb0a0bdc0e
Myriad-Dreamin <camiyoru@gmail.com>
2023-12-23 14:20:33 +0800

    dev(m_engine): test macro performance

--- crates/mitex-parser/benches/simple.rs
+++ crates/mitex-parser/benches/simple.rs
@@ -1,5 +1,5 @@
 use divan::{AllocProfiler, Bencher};
-use mitex_parser::{parse, CommandSpec};
+use mitex_parser::{parse, parser::parse_with_macro, CommandSpec};
 
 #[global_allocator]
 static ALLOC: AllocProfiler = AllocProfiler::system();
@@ -40,6 +40,10 @@ fn bench(bencher: Bencher, input: &str, spec: CommandSpec) {
     bencher.bench(|| parse(input, spec.clone()));
 }
 
+fn bench_macro(bencher: Bencher, input: &str, spec: CommandSpec) {
+    bencher.bench(|| parse_with_macro(input, spec.clone()));
+}
+
 #[divan::bench]
 fn alpha_x_20000(bencher: Bencher) {
     bench(bencher, &ALPHA_X_20000, DEFAULT_SPEC.clone());
@@ -48,6 +52,11 @@ fn alpha_x_20000(bencher: Bencher) {
 #[divan::bench]
 fn alpha_x_40000(bencher: Bencher) {
     bench(bencher, &ALPHA_X_40000, DEFAULT_SPEC.clone());
+}
+
+#[divan::bench]
+fn alpha_x_40000_macro(bencher: Bencher) {
+    bench_macro(bencher, &ALPHA_X_40000, DEFAULT_SPEC.clone());
 }
 
 #[divan::bench]
@@ -84,77 +93,102 @@ fn starred_command(bencher: Bencher) {
     bench(bencher, &STARRED_COMMAND, DEFAULT_SPEC.clone());
 }
 
+#[divan::bench]
+fn starred_command_macro(bencher: Bencher) {
+    bench_macro(bencher, &STARRED_COMMAND, DEFAULT_SPEC.clone());
+}
+
 /*
 last^1
-simple                  fastest       │ slowest       │ median        │ mean          │ samples │ iters
-├─ alpha_x_20000        2.198 ms      │ 3.16 ms       │ 2.355 ms      │ 2.44 ms       │ 100     │ 100
-│                       alloc:        │               │               │               │         │
-│                         12          │ 12            │ 12            │ 12            │         │
-│                         960.9 KB    │ 960.9 KB      │ 960.9 KB      │ 960.9 KB      │         │
-│                       dealloc:      │               │               │               │         │
-│                         5           │ 5             │ 5             │ 5             │         │
-│                         1.579 MB    │ 1.579 MB      │ 1.579 MB      │ 1.579 MB      │         │
-│                       grow:         │               │               │               │         │
-│                         18          │ 18            │ 18            │ 18            │         │
-│                         1.578 MB    │ 1.578 MB      │ 1.578 MB      │ 1.578 MB      │         │
-├─ alpha_x_20000_heavy  2.215 ms      │ 3.235 ms      │ 2.394 ms      │ 2.435 ms      │ 100     │ 100
-│                       alloc:        │               │               │               │         │
-│                         12          │ 12            │ 12            │ 12            │         │
-│                         960.9 KB    │ 960.9 KB      │ 960.9 KB      │ 960.9 KB      │         │
-│                       dealloc:      │               │               │               │         │
-│                         5           │ 5             │ 5             │ 5             │         │
-│                         1.579 MB    │ 1.579 MB      │ 1.579 MB      │ 1.579 MB      │         │
-│                       grow:         │               │               │               │         │
-│                         18          │ 18            │ 18            │ 18            │         │
-│                         1.578 MB    │ 1.578 MB      │ 1.578 MB      │ 1.578 MB      │         │
-├─ alpha_x_40000        4.605 ms      │ 7.2 ms        │ 4.851 ms      │ 4.969 ms      │ 100     │ 100
-│                       alloc:        │               │               │               │         │
-│                         12          │ 12            │ 12            │ 12            │         │
-│                         1.92 MB     │ 1.92 MB       │ 1.92 MB       │ 1.92 MB       │         │
-│                       dealloc:      │               │               │               │         │
-│                         5           │ 5             │ 5             │ 5             │         │
-│                         3.152 MB    │ 3.152 MB      │ 3.152 MB      │ 3.152 MB      │         │
-│                       grow:         │               │               │               │         │
-│                         19          │ 19            │ 19            │ 19            │         │
-│                         3.151 MB    │ 3.151 MB      │ 3.151 MB      │ 3.151 MB      │         │
-├─ alpha_x_40000_heavy  4.594 ms      │ 6.76 ms       │ 4.918 ms      │ 5.035 ms      │ 100     │ 100
-│                       alloc:        │               │               │               │         │
-│                         12          │ 12            │ 12            │ 12            │         │
-│                         1.92 MB     │ 1.92 MB       │ 1.92 MB       │ 1.92 MB       │         │
-│                       dealloc:      │               │               │               │         │
-│                         5           │ 5             │ 5             │ 5             │         │
-│                         3.152 MB    │ 3.152 MB      │ 3.152 MB      │ 3.152 MB      │         │
-│                       grow:         │               │               │               │         │
-│                         19          │ 19            │ 19            │ 19            │         │
-│                         3.151 MB    │ 3.151 MB      │ 3.151 MB      │ 3.151 MB      │         │
-├─ slice_word           2.696 ms      │ 4.247 ms      │ 3.024 ms      │ 3.09 ms       │ 100     │ 100
-│                       alloc:        │               │               │               │         │
-│                         12          │ 12            │ 12            │ 12            │         │
-│                         960.9 KB    │ 960.9 KB      │ 960.9 KB      │ 960.9 KB      │         │
-│                       dealloc:      │               │               │               │         │
-│                         5           │ 5             │ 5             │ 5             │         │
-│                         1.579 MB    │ 1.579 MB      │ 1.579 MB      │ 1.579 MB      │         │
-│                       grow:         │               │               │               │         │
-│                         18          │ 18            │ 18            │ 18            │         │
-│                         1.578 MB    │ 1.578 MB      │ 1.578 MB      │ 1.578 MB      │         │
-├─ sqrt_pattern         2.07 ms       │ 4.043 ms      │ 2.28 ms       │ 2.384 ms      │ 100     │ 100
-│                       alloc:        │               │               │               │         │
-│                         7523        │ 7523          │ 7523          │ 7523          │         │
-│                         1.021 MB    │ 1.021 MB      │ 1.021 MB      │ 1.021 MB      │         │
-│                       dealloc:      │               │               │               │         │
-│                         8           │ 8             │ 8             │ 8             │         │
-│                         793 KB      │ 793 KB        │ 793 KB        │ 793 KB        │         │
-│                       grow:         │               │               │               │         │
-│                         17          │ 17            │ 17            │ 17            │         │
-│                         792 KB      │ 792 KB        │ 792 KB        │ 792 KB        │         │
-╰─ starred_command      1.035 ms      │ 4.854 ms      │ 1.304 ms      │ 1.54 ms       │ 100     │ 100
-                        alloc:        │               │               │               │         │
-                          18          │ 18            │ 18            │ 18            │         │
-                          241.2 KB    │ 241.2 KB      │ 241.2 KB      │ 241.2 KB      │         │
-                        dealloc:      │               │               │               │         │
-                          7           │ 7             │ 7             │ 7             │         │
-                          399.7 KB    │ 399.7 KB      │ 399.7 KB      │ 399.7 KB      │         │
-                        grow:         │               │               │               │         │
-                          16          │ 16            │ 16            │ 16            │         │
-                          398.8 KB    │ 398.8 KB      │ 398.8 KB      │ 398.8 KB      │         │
+simple                    fastest       │ slowest       │ median        │ mean          │ samples │ iters
+├─ alpha_x_20000          1.957 ms      │ 3.166 ms      │ 2.169 ms      │ 2.243 ms      │ 100     │ 100
+│                         alloc:        │               │               │               │         │
+│                           12          │ 12            │ 12            │ 12            │         │
+│                           960.9 KB    │ 960.9 KB      │ 960.9 KB      │ 960.9 KB      │         │
+│                         dealloc:      │               │               │               │         │
+│                           5           │ 5             │ 5             │ 5             │         │
+│                           1.579 MB    │ 1.579 MB      │ 1.579 MB      │ 1.579 MB      │         │
+│                         grow:         │               │               │               │         │
+│                           18          │ 18            │ 18            │ 18            │         │
+│                           1.578 MB    │ 1.578 MB      │ 1.578 MB      │ 1.578 MB      │         │
+├─ alpha_x_20000_heavy    2.026 ms      │ 3.064 ms      │ 2.282 ms      │ 2.362 ms      │ 100     │ 100
+│                         alloc:        │               │               │               │         │
+│                           12          │ 12            │ 12            │ 12            │         │
+│                           960.9 KB    │ 960.9 KB      │ 960.9 KB      │ 960.9 KB      │         │
+│                         dealloc:      │               │               │               │         │
+│                           5           │ 5             │ 5             │ 5             │         │
+│                           1.579 MB    │ 1.579 MB      │ 1.579 MB      │ 1.579 MB      │         │
+│                         grow:         │               │               │               │         │
+│                           18          │ 18            │ 18            │ 18            │         │
+│                           1.578 MB    │ 1.578 MB      │ 1.578 MB      │ 1.578 MB      │         │
+├─ alpha_x_40000          4.328 ms      │ 6.038 ms      │ 4.66 ms       │ 4.766 ms      │ 100     │ 100
+│                         alloc:        │               │               │               │         │
+│                           12          │ 12            │ 12            │ 12            │         │
+│                           1.92 MB     │ 1.92 MB       │ 1.92 MB       │ 1.92 MB       │         │
+│                         dealloc:      │               │               │               │         │
+│                           5           │ 5             │ 5             │ 5             │         │
+│                           3.152 MB    │ 3.152 MB      │ 3.152 MB      │ 3.152 MB      │         │
+│                         grow:         │               │               │               │         │
+│                           19          │ 19            │ 19            │ 19            │         │
+│                           3.151 MB    │ 3.151 MB      │ 3.151 MB      │ 3.151 MB      │         │
+├─ alpha_x_40000_heavy    4.485 ms      │ 6.775 ms      │ 4.962 ms      │ 5.033 ms      │ 100     │ 100
+│                         alloc:        │               │               │               │         │
+│                           12          │ 12            │ 12            │ 12            │         │
+│                           1.92 MB     │ 1.92 MB       │ 1.92 MB       │ 1.92 MB       │         │
+│                         dealloc:      │               │               │               │         │
+│                           5           │ 5             │ 5             │ 5             │         │
+│                           3.152 MB    │ 3.152 MB      │ 3.152 MB      │ 3.152 MB      │         │
+│                         grow:         │               │               │               │         │
+│                           19          │ 19            │ 19            │ 19            │         │
+│                           3.151 MB    │ 3.151 MB      │ 3.151 MB      │ 3.151 MB      │         │
+├─ alpha_x_40000_macro    4.616 ms      │ 7.79 ms       │ 5.089 ms      │ 5.19 ms       │ 100     │ 100
+│                         alloc:        │               │               │               │         │
+│                           57          │ 57            │ 57            │ 57            │         │
+│                           1.924 MB    │ 1.924 MB      │ 1.924 MB      │ 1.924 MB      │         │
+│                         dealloc:      │               │               │               │         │
+│                           50          │ 50            │ 50            │ 50            │         │
+│                           3.156 MB    │ 3.156 MB      │ 3.156 MB      │ 3.156 MB      │         │
+│                         grow:         │               │               │               │         │
+│                           22          │ 22            │ 22            │ 22            │         │
+│                           3.152 MB    │ 3.152 MB      │ 3.152 MB      │ 3.152 MB      │         │
+├─ slice_word             2.057 ms      │ 3.94 ms       │ 2.314 ms      │ 2.411 ms      │ 100     │ 100
+│                         alloc:        │               │               │               │         │
+│                           12          │ 12            │ 12            │ 12            │         │
+│                           960.9 KB    │ 960.9 KB      │ 960.9 KB      │ 960.9 KB      │         │
+│                         dealloc:      │               │               │               │         │
+│                           5           │ 5             │ 5             │ 5             │         │
+│                           1.579 MB    │ 1.579 MB      │ 1.579 MB      │ 1.579 MB      │         │
+│                         grow:         │               │               │               │         │
+│                           18          │ 18            │ 18            │ 18            │         │
+│                           1.578 MB    │ 1.578 MB      │ 1.578 MB      │ 1.578 MB      │         │
+├─ sqrt_pattern           1.778 ms      │ 4.774 ms      │ 2.04 ms       │ 2.225 ms      │ 100     │ 100
+│                         alloc:        │               │               │               │         │
+│                           7523        │ 7523          │ 7523          │ 7523          │         │
+│                           1.021 MB    │ 1.021 MB      │ 1.021 MB      │ 1.021 MB      │         │
+│                         dealloc:      │               │               │               │         │
+│                           8           │ 8             │ 8             │ 8             │         │
+│                           793 KB      │ 793 KB        │ 793 KB        │ 793 KB        │         │
+│                         grow:         │               │               │               │         │
+│                           17          │ 17            │ 17            │ 17            │         │
+│                           792 KB      │ 792 KB        │ 792 KB        │ 792 KB        │         │
+├─ starred_command        836.4 µs      │ 1.635 ms      │ 1.01 ms       │ 1.054 ms      │ 100     │ 100
+│                         alloc:        │               │               │               │         │
+│                           18          │ 18            │ 18            │ 18            │         │
+│                           241.2 KB    │ 241.2 KB      │ 241.2 KB      │ 241.2 KB      │         │
+│                         dealloc:      │               │               │               │         │
+│                           7           │ 7             │ 7             │ 7             │         │
+│                           399.7 KB    │ 399.7 KB      │ 399.7 KB      │ 399.7 KB      │         │
+│                         grow:         │               │               │               │         │
+│                           16          │ 16            │ 16            │ 16            │         │
+│                           398.8 KB    │ 398.8 KB      │ 398.8 KB      │ 398.8 KB      │         │
+╰─ starred_command_macro  927.8 µs      │ 1.705 ms      │ 1.01 ms       │ 1.059 ms      │ 100     │ 100
+                          alloc:        │               │               │               │         │
+                            63          │ 63            │ 63            │ 63            │         │
+                            244.5 KB    │ 244.5 KB      │ 244.5 KB      │ 244.5 KB      │         │
+                          dealloc:      │               │               │               │         │
+                            52          │ 52            │ 52            │ 52            │         │
+                            404.1 KB    │ 404.1 KB      │ 404.1 KB      │ 404.1 KB      │         │
+                          grow:         │               │               │               │         │
+                            19          │ 19            │ 19            │ 19            │         │
+                            400 KB      │ 400 KB        │ 400 KB        │ 400 KB        │         │
  */
