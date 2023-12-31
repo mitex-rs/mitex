077ee207a3662dc2bcae4249238e0baba8265375
mgt <mgt@oi-wiki.org>
2023-12-31 16:36:57 +0800

    add build script for mitex web

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,3 +1,7 @@
 cargo run --release --bin mitex
 cargo build --release --target wasm32-unknown-unknown -p mitex-typst
 cp target/wasm32-unknown-unknown/release/mitex_typst.wasm packages/mitex/mitex.wasm
+
+cd crates/mitex-web-wasm
+wasm-pack build --release
+cd ../..
--- scripts/build.sh
+++ scripts/build.sh
@@ -2,3 +2,7 @@
 cargo run --release --bin mitex
 cargo build --release --target wasm32-unknown-unknown -p mitex-typst
 cp target/wasm32-unknown-unknown/release/mitex_typst.wasm packages/mitex/mitex.wasm
+
+pushd crates\mitex-web-wasm
+wasm-pack build --release
+popd
