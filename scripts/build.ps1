22c3ed6363bbd6401867e36dc8ee1f50e94603ea
mgt <mgt@oi-wiki.org>
2023-12-31 15:46:38 +0800

    adjust build script

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,4 +1,3 @@
 cargo run --release --bin mitex
 cargo build --release --target wasm32-unknown-unknown -p mitex-typst
-rm packages/mitex/mitex.wasm -Force
-mv target/wasm32-unknown-unknown/release/mitex_typst.wasm packages/mitex/mitex.wasm
+cp target/wasm32-unknown-unknown/release/mitex_typst.wasm packages/mitex/mitex.wasm
