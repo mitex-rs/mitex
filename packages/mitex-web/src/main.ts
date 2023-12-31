01b57170e0fd8d2547cd225f44e2191554dcdc6b
mgt <mgt@oi-wiki.org>
2023-12-31 17:04:52 +0800

    add checkbox for template and change error style

--- packages/mitex-web/src/main.ts
+++ packages/mitex-web/src/main.ts
@@ -1,15 +1,34 @@
 import './style.css'
 import van from "vanjs-core"
 import { convert_math } from "mitex-web-wasm"
-const { div, textarea } = van.tags
+const { div, textarea, input } = van.tags
 
 const App = () => {
-  const input = textarea({ placeholder: "Type LaTeX math equations here", autofocus: true, rows: 10 })
+  const input_area = textarea({ placeholder: "Type LaTeX math equations here", autofocus: true, rows: 10 })
+  const template_checkbox = input({ checked: true, type: "checkbox", name: "With typst templates" })
+  const import_checkbox = input({ checked: false, type: "checkbox", name: "With mitex imports" })
+  const checkbox_container = div(
+    div(
+      template_checkbox,
+      "With typst templates"
+    ),
+    div(
+      import_checkbox,
+      "With mitex imports"
+    )
+  )
   const output = textarea({ readOnly: true, placeholder: "Output", rows: 10 })
   const error = div({ class: "error" })
-  input.oninput = () => {
+  const update_output = () => {
     try {
-      output.value = convert_math(input.value, new Uint8Array)
+      let convert_res = convert_math(input_area.value, new Uint8Array)
+      if (template_checkbox.checked) {
+        convert_res = `#math.equation(eval("$" + "${convert_res}" + "$", scope: mitex-scope))`
+      }
+      if (import_checkbox.checked) {
+        convert_res = `#import "@preview/mitex:0.1.0": *` + "\n\n" + convert_res
+      }
+      output.value = convert_res
       error.textContent = ""
     } catch (e) {
       output.value = ""
@@ -17,9 +36,13 @@ const App = () => {
     }
   }
   output.onfocus = () => output.select()
+  input_area.oninput = update_output
+  template_checkbox.onchange = update_output
+  import_checkbox.onchange = update_output
   return div(
-    input,
+    input_area,
     output,
+    checkbox_container,
     error
   )
 }
