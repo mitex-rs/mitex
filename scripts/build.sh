e4ec6b3363d300bfc597268c19f1aec075cc45b9
mgt <mgt@oi-wiki.org>
2023-12-31 17:45:47 +0800

    fix build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,6 +3,6 @@ cargo run --release --bin mitex
 cargo build --release --target wasm32-unknown-unknown -p mitex-typst
 cp target/wasm32-unknown-unknown/release/mitex_typst.wasm packages/mitex/mitex.wasm
 
-pushd crates\mitex-web-wasm
+cd crates/mitex-web-wasm
 wasm-pack build --release
-popd
+cd ../..
