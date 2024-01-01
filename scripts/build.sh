5cfdb7cdf3b779edd9eceff6ac06f7ec065275d7
mgt <mgt@oi-wiki.org>
2024-01-01 15:11:00 +0800

    no more sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,6 +3,6 @@ set -euxo pipefail
 cargo build --release --target wasm32-unknown-unknown --manifest-path ./crates/mitex-wasm/Cargo.toml --features typst-plugin
 cp target/wasm32-unknown-unknown/release/mitex_wasm.wasm packages/mitex/mitex.wasm
 
-cd crates/mitex-wasm
+pushd crates/mitex-wasm
 wasm-pack build --release --features web
-cd ../..
+popd
