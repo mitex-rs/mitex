6094f0e85cbea3e6752bbf0bc22f130f126b5dd7
mgt <mgt@oi-wiki.org>
2024-01-01 15:05:07 +0800

    add pipefail to build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -euxo pipefail
 cargo build --release --target wasm32-unknown-unknown --manifest-path ./crates/mitex-wasm/Cargo.toml --features typst-plugin
 cp target/wasm32-unknown-unknown/release/mitex_wasm.wasm packages/mitex/mitex.wasm
 
