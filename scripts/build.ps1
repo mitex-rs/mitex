42e5398be2f1a12945b58c4bc4ea5126da96ed2e
Myriad-Dreamin <camiyoru@gmail.com>
2024-01-01 14:38:07 +0800

    fix: build scripts

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -3,6 +3,6 @@ $InstallPath = "packages/mitex/mitex.wasm"
 if (Test-Path $InstallPath) {
   Remove-Item $InstallPath
 }
-mv target/wasm32-unknown-unknown/release/mitex_typst.wasm $InstallPath
+mv target/wasm32-unknown-unknown/release/mitex_wasm.wasm $InstallPath
 
 pwsh -Command { cd crates/mitex-wasm; wasm-pack build --release --features web }
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 cargo build --release --target wasm32-unknown-unknown --manifest-path ./crates/mitex-wasm/Cargo.toml --features typst-plugin
-cp target/wasm32-unknown-unknown/release/mitex_typst.wasm packages/mitex/mitex.wasm
+cp target/wasm32-unknown-unknown/release/mitex_wasm.wasm packages/mitex/mitex.wasm
 
 cd crates/mitex-wasm
 wasm-pack build --release --features web
