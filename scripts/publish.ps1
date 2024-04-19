f556e946bd71c84925f2237513ebb83b8c7d8113
Myriad-Dreamin <camiyoru@gmail.com>
2024-04-19 13:23:45 +0800

    build: update publish script

---
+++ scripts/publish.ps1
@@ -0,0 +1,8 @@
+cargo publish -p mitex-glob
+cargo publish -p mitex-spec
+cargo publish -p mitex-spec-gen
+cargo publish -p mitex-lexer
+cargo publish -p mitex-parser
+cargo publish -p mitex
+# cargo publish -p mitex-wasm
+cargo publish -p mitex-cli
