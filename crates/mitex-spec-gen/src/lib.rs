c6c5f689a914eeddcbd41deefe1b61398d8db07b
Myriad-Dreamin <35292584+Myriad-Dreamin@users.noreply.github.com>
2024-04-19 16:38:04 +0800

    docs: refactor and update docs of mitex-{spec-gen,wasm} (#158)

--- crates/mitex-spec-gen/src/lib.rs
+++ crates/mitex-spec-gen/src/lib.rs
@@ -1,8 +1,13 @@
-//! Generate Default Command Specification for embedded LaTeX packages.
+//! Provides embedded command specifications for MiTeX.
 
 use mitex_spec::CommandSpec;
 
-/// Default Command Specification.
+/// The default command specification.
+///
+/// See [Reproducing Default Command Specification][repro-default] for more
+/// information.
+///
+/// [repro-default]: https://github.com/mitex-rs/artifacts/blob/main/README.md#default-command-specification-since-v011
 pub static DEFAULT_SPEC: once_cell::sync::Lazy<CommandSpec> = once_cell::sync::Lazy::new(|| {
     CommandSpec::from_bytes(include_bytes!(
         "../../../target/mitex-artifacts/spec/default.rkyv"
