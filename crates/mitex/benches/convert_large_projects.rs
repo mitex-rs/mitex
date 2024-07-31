d88792d2b0808dc61ee21c02c22a47affe2cc13d
Myriad-Dreamin <camiyoru@gmail.com>
2024-08-01 00:13:03 +0800

    build: update bench data

--- crates/mitex/benches/convert_large_projects.rs
+++ crates/mitex/benches/convert_large_projects.rs
@@ -59,36 +59,38 @@ fn oiwiki_231222_macro(bencher: Bencher) {
 
 last^1 (macro support, typst v0.10.0)
 Benchmark 1: typst compile --root . crates\mitex\benches\empty.typ
-  Time (mean ± σ):     399.6 ms ±  36.6 ms    [User: 75.0 ms, System: 26.6 ms]
-  Range (min … max):   371.0 ms … 495.5 ms    10 runs
+  Time (mean ± σ):      79.9 ms ±   3.0 ms    [User: 6.1 ms, System: 6.9 ms]
+  Range (min … max):    75.2 ms …  88.5 ms    36 runs
+
 Benchmark 1: typst compile --root . crates\mitex\benches\oiwiki.typ
-  Time (mean ± σ):      2.179 s ±  0.029 s    [User: 0.681 s, System: 0.049 s]
-  Range (min … max):    2.142 s …  2.229 s    10 runs
+  Time (mean ± σ):      1.526 s ±  0.070 s    [User: 0.473 s, System: 0.028 s]
+  Range (min … max):    1.418 s …  1.634 s    10 runs
+
 Benchmark 1: typst compile --root . crates\mitex\benches\oiwiki-with-render.typ
-  Time (mean ± σ):      3.830 s ±  0.021 s    [User: 1.223 s, System: 0.168 s]
-  Range (min … max):    3.787 s …  3.867 s    10 runs
+  Time (mean ± σ):      3.155 s ±  0.161 s    [User: 1.001 s, System: 0.082 s]
+  Range (min … max):    3.016 s …  3.563 s    10 runs
 
 convert_large_projects  fastest       │ slowest       │ median        │ mean          │ samples │ iters
-├─ oiwiki_231222        79.07 ms      │ 87.49 ms      │ 80.94 ms      │ 81.26 ms      │ 100     │ 100
+├─ oiwiki_231222        83.09 ms      │ 104.7 ms      │ 87.86 ms      │ 88.71 ms      │ 100     │ 100
 │                       alloc:        │               │               │               │         │
-│                         1361478     │ 1361478       │ 1361478       │ 1361478       │         │
-│                         84.71 MB    │ 84.71 MB      │ 84.71 MB      │ 84.71 MB      │         │
+│                         1398801     │ 1398801       │ 1398801       │ 1398801       │         │
+│                         85.32 MB    │ 85.32 MB      │ 85.32 MB      │ 85.32 MB      │         │
 │                       dealloc:      │               │               │               │         │
-│                         1361478     │ 1361478       │ 1361478       │ 1361478       │         │
-│                         93.27 MB    │ 93.27 MB      │ 93.27 MB      │ 93.27 MB      │         │
+│                         1398801     │ 1398801       │ 1398801       │ 1398801       │         │
+│                         95.64 MB    │ 95.64 MB      │ 95.64 MB      │ 95.64 MB      │         │
 │                       grow:         │               │               │               │         │
-│                         64098       │ 64098         │ 64098         │ 64098         │         │
-│                         8.556 MB    │ 8.556 MB      │ 8.556 MB      │ 8.556 MB      │         │
-╰─ oiwiki_231222_macro  79.46 ms      │ 140.6 ms      │ 84.31 ms      │ 85.6 ms       │ 100     │ 100
+│                         71029       │ 71029         │ 71029         │ 71029         │         │
+│                         10.31 MB    │ 10.31 MB      │ 10.31 MB      │ 10.31 MB      │         │
+╰─ oiwiki_231222_macro  81.95 ms      │ 119 ms        │ 87.38 ms      │ 88.42 ms      │ 100     │ 100
                         alloc:        │               │               │               │         │
-                          1361478     │ 1361478       │ 1361478       │ 1361478       │         │
-                          84.71 MB    │ 84.71 MB      │ 84.71 MB      │ 84.71 MB      │         │
+                          1398801     │ 1398801       │ 1398801       │ 1398801       │         │
+                          85.32 MB    │ 85.32 MB      │ 85.32 MB      │ 85.32 MB      │         │
                         dealloc:      │               │               │               │         │
-                          1361478     │ 1361478       │ 1361478       │ 1361478       │         │
-                          93.27 MB    │ 93.27 MB      │ 93.27 MB      │ 93.27 MB      │         │
+                          1398801     │ 1398801       │ 1398801       │ 1398801       │         │
+                          95.64 MB    │ 95.64 MB      │ 95.64 MB      │ 95.64 MB      │         │
                         grow:         │               │               │               │         │
-                          64098       │ 64098         │ 64098         │ 64098         │         │
-                          8.556 MB    │ 8.556 MB      │ 8.556 MB      │ 8.556 MB      │         │
+                          71029       │ 71029         │ 71029         │ 71029         │         │
+                          10.31 MB    │ 10.31 MB      │ 10.31 MB      │ 10.31 MB      │         │
 
 baseline (typst v0.10.0)
 Benchmark 1: typst compile --root . crates\mitex\benches\empty.typ
--- packages/mitex/examples/bench.typ
+++ packages/mitex/examples/bench.typ
@@ -8,10 +8,20 @@ Write inline equations like #mi("x") or #mi[y].
 
 Also block equations:
 
-#mitex("\alpha x" * 8000)
+#mitex("\alpha x" * 17000)
 
 /*
 last^1
+17000
+Benchmark 1: typst compile --root . packages\mitex\examples\bench.typ
+  Time (mean ± σ):     337.0 ms ±  12.7 ms    [User: 31.2 ms, System: 4.7 ms]
+  Range (min … max):   321.9 ms … 361.7 ms    10 runs
+8000
+Benchmark 1: typst compile --root . packages\mitex\examples\bench.typ
+  Time (mean ± σ):     209.0 ms ±  10.6 ms    [User: 42.4 ms, System: 19.0 ms]
+  Range (min … max):   197.0 ms … 230.0 ms    14 runs
+
+last^2
 17000
 Benchmark 1: typst compile --root . packages\mitex\examples\bench.typ
   Time (mean ± σ):     638.8 ms ±  10.4 ms    [User: 143.8 ms, System: 32.8 ms]
@@ -20,16 +30,6 @@ Benchmark 1: typst compile --root . packages\mitex\examples\bench.typ
 Benchmark 1: typst compile --root . packages\mitex\examples\bench.typ
   Time (mean ± σ):     503.2 ms ±  15.1 ms    [User: 109.4 ms, System: 28.1 ms]
   Range (min … max):   485.8 ms … 535.5 ms    10 runs
-
-last^2
-17000
-Benchmark 1: typst compile --root . typst-package\examples\bench.typ
-  Time (mean ± σ):     647.5 ms ±  16.3 ms    [User: 109.4 ms, System: 23.4 ms]
-  Range (min … max):   630.9 ms … 676.8 ms    10 runs
-8000
-Benchmark 1: typst compile --root . typst-package\examples\bench.typ
-  Time (mean ± σ):     537.5 ms ±  23.3 ms    [User: 76.6 ms, System: 17.2 ms]
-  Range (min … max):   509.1 ms … 581.7 ms    10 runs
 
 init
 17000
